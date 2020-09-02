15f209bb84f6400291e62e80c59d0f0ddb0ab58d
Artemona <artemidaabc@gmail.com>
2020-09-02 21:56:53 +0300

    fix containerMedia and js code

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,18 @@ const open = document.querySelector('.menu-icon');
 const menu = document.querySelector('.nav__menu');
 const close = document.querySelector('.close-menu-icon');
 
-
 buttonsMobile.addEventListener('click', function (e) {
    let currentTarget = e.target;
    open.classList.remove('active');
    menu.classList.add('active');
    close.classList.remove('disable');
    currentTarget.closest("div").classList.add('disable');
-   if(close.classList.contains('disable')) {
+   if (close.classList.contains('disable')) {
       menu.classList.remove('active');
       open.classList.remove('disable')
+   }
+   if (document.documentElement.clientWidth < 480) {
+      menu.style.left = `${document.documentElement.clientWidth - 330}px`;
    }
 });
 
@@ -21,7 +23,7 @@ document.addEventListener('click', function (e) {
       return false
    }
    menu.classList.remove('active');
-   if(!close.classList.contains('active')) {
+   if (!close.classList.contains('active')) {
       close.classList.add('disable');
       open.classList.add('active')
    }
